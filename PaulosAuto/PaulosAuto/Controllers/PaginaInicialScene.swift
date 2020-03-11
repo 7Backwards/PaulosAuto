eae02fcb0a53d034556e0527f7478e205179ceeb
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-03-11 23:40:52 +0000

    Fixed logo size on nav bar

--- PaulosAuto/PaulosAuto/Controllers/PaginaInicialScene.swift
+++ PaulosAuto/PaulosAuto/Controllers/PaginaInicialScene.swift
@@ -9,35 +9,14 @@
 import UIKit
 
 class PaginaInicialScene: ViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-       addNavBarImage()
+        super.addNavBarImage()
     }
     
-    func addNavBarImage() {
-           let imageView = UIImageView(image: #imageLiteral(resourceName: "Paulosauto-2"))
-           imageView.frame = CGRect(x: 0, y: 0, width: 170, height: 30)
-           imageView.contentMode = .scaleAspectFit
-
-           let titleView = UIView(frame: CGRect(x: 0, y: 0, width: 170, height: 30))
-
-           titleView.addSubview(imageView)
-           titleView.backgroundColor = .clear
-           self.navigationItem.titleView = titleView
-           }
+    
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- PaulosAuto/PaulosAuto/Controllers/ViewController.swift
+++ PaulosAuto/PaulosAuto/Controllers/ViewController.swift
@@ -11,11 +11,23 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         debugPrint("viewDidLoad")
+    }
+    
+    func addNavBarImage() {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Paulosauto-2"))
+        imageView.frame = CGRect(x: 0, y: -20, width: 200, height: 70)
+        imageView.contentMode = .scaleAspectFit
+        
+        let titleView = UIView(frame: CGRect(x: 0, y: -20, width: 200, height: 70))
+        
+        titleView.addSubview(imageView)
+        titleView.backgroundColor = .clear
+        self.navigationItem.titleView = titleView
     }
     
     
