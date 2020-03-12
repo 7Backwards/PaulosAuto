9c43ba6a08087c7c1afa95c1e6590c1a8d83471c
Gonçalo Neves <goncalo.filipe.neves@pt.softinsa.com>
2020-03-12 18:55:56 +0000

    Add collectionView

---
+++ PaulosAuto/PaulosAuto/Controllers/CollectionViewEquipamentos.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewEquipamentos.swift
+//  PaulosAuto
+//
+//  Created by Gonçalo Neves on 12/03/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewEquipamentos: UICollectionViewCell {
+    
+    @IBOutlet weak var CellView: UIView!
+}
