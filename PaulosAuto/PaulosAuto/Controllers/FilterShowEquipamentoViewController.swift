a53342e67aefdc5ff4f54facdfa94ba547649294
Gonçalo Neves <goncalo.filipe.neves@pt.softinsa.com>
2020-03-27 23:01:08 +0000

    Added Filter View Controller

---
+++ PaulosAuto/PaulosAuto/Controllers/FilterShowEquipamentoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FilterShowEquipamentoViewController.swift
+//  PaulosAuto
+//
+//  Created by Gonçalo Neves on 27/03/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import UIKit
+
+class FilterShowEquipamentoViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PaulosAuto/PaulosAuto/Extensions/UIView+Snapshot.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+Snapshot.swift
+//  PaulosAuto
+//
+//  Created by Gonçalo Neves on 27/03/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import Foundation
