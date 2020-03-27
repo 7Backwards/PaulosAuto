ea49f1cae38976730a928a2e78c9295701cb0b5d
Gonçalo Neves <goncalo.filipe.neves@pt.softinsa.com>
2020-03-27 01:14:23 +0000

    Add Reportar Problema View Controller

---
+++ PaulosAuto/PaulosAuto/Controllers/ReportarProblemaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReportarProblemaViewController.swift
+//  PaulosAuto
+//
+//  Created by Gonçalo Neves on 26/03/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import UIKit
+
+class ReportarProblemaViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
