147b368559e159c61b467cb9c086b36af82b3607
Gonçalo Neves <goncalo.filipe.neves@pt.softinsa.com>
2020-04-17 02:39:54 +0100

    Created DataDelegate to allow passing of values between different ViewControllers without segue

---
+++ PaulosAuto/PaulosAuto/Protocols/DataDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  DataDelegate.swift
+//  PaulosAuto
+//
+//  Created by Gonçalo Neves on 17/04/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+protocol DataDelegate {
+    
+    func updateActiveCategoryFiltered(newActiveCategory: String)
+    func updateActiveOrderByFiltered(newOrderBy: Int)
+}
