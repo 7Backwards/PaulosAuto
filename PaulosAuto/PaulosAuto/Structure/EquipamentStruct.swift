c9ac8f42d9682c2ef4d00c1ba738529e579a2a4b
Gonçalo Neves <goncalo.filipe.neves@pt.softinsa.com>
2020-04-17 18:45:28 +0100

    Changed data types to Data from String to support order by

--- PaulosAuto/PaulosAuto/Structure/EquipamentStruct.swift
+++ PaulosAuto/PaulosAuto/Structure/EquipamentStruct.swift
@@ -22,16 +22,16 @@ struct Equipment: Decodable {
     var currentHours : Int?
     var image : String?
     var year : Int?
-    var dateAssignment : String?
-    var dateStartAssignment : String?
-    var dateEndAssignment : String?
+    var dateAssignment : Date?
+    var dateStartAssignment : Date?
+    var dateEndAssignment : Date?
     var smp : Bool?
     
     
     // MARK: - Functions
     
     
-    init(brand : String?,model : String?,type : String?,serialNumber : String?,plate : String?,currentHours : Int?,image : String?,year : Int?,dateAssignment : String?,dateStartAssignment : String?,dateEndAssignment : String?,smp : Bool?){
+    init(brand : String,model : String,type : String,serialNumber : String,plate : String,currentHours : Int,image : String,year : Int,dateAssignment : String,dateStartAssignment : String,dateEndAssignment : String,smp : Bool){
         
         self.brand = brand
         self.model = model
@@ -41,9 +41,26 @@ struct Equipment: Decodable {
         self.currentHours = currentHours
         self.image = image
         self.year = year
-        self.dateAssignment = dateAssignment
-        self.dateStartAssignment = dateStartAssignment
-        self.dateEndAssignment = dateEndAssignment
         self.smp = smp
+        
+        // Set the date formatter and optionally set the formatted date from string
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        if let date = dateFormatter.date(from: dateAssignment) {
+            self.dateAssignment = date
+            
+        }
+        if let date = dateFormatter.date(from: dateStartAssignment) {
+            self.dateStartAssignment = date
+            
+        }
+        if let date = dateFormatter.date(from: dateEndAssignment) {
+            self.dateEndAssignment = date
+            
+        }
+        
+        
     }
+    
+    
 }
