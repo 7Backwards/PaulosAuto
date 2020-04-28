5ac5dd70480785bd5ad31c8a634407be8ee4bca5
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-04-29 00:01:45 +0100

    Create file structure to list invoices

---
+++ PaulosAuto/PaulosAuto/Controllers/InvoiceViewController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  InvoiceViewController+Extensions.swift
+//  PaulosAuto
+//
+//  Created by Gonçalo Neves on 28/04/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import Foundation
