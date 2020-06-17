9de4d4586b8d02eba39d2901f3b77c4532cd6992
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-06-17 20:00:20 +0100

    Created file structure for list reported problems

---
+++ PaulosAuto/PaulosAuto/Controllers/ReportProblemInfoViewController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  ReportProblemInfoViewController+Extensions.swift
+//  PaulosAuto
+//
+//  Created by Neves on 17/06/2020.
+//  Copyright © 2020 Gonçalo Neves. All rights reserved.
+//
+
+import Foundation
